f6f8000d263d13554400f201e9155294e57a9425
Jovica <jovica.djurisic@gmail.com>
2022-07-23 10:41:32 +0200

    Project init.

---
+++ int-task-be/src/main/java/com/example/inttaskbe/IntTaskBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.inttaskbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntTaskBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntTaskBeApplication.class, args);
+    }
+
+}
