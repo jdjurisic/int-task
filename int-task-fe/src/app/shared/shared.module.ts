9af1c5d13ffefcabb3801f299d363d6821485b94
Jovica <jovica.djurisic@gmail.com>
2022-07-24 21:56:22 +0200

    Created header and toast component, toast service.

--- int-task-fe/src/app/shared/shared.module.ts
+++ int-task-fe/src/app/shared/shared.module.ts
@@ -1,12 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbNavModule, NgbPaginationModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { GlobalToastComponent } from './components/global-toast/global-toast.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    GlobalToastComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NgbNavModule,
+    RouterModule,
+    HttpClientModule,
+    NgbPaginationModule,
+    NgbNavModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbToastModule
+  ],
+  exports: [
+    HeaderComponent,
+    NgbNavModule,
+    RouterModule,
+    HttpClientModule,
+    NgbPaginationModule,
+    NgbNavModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbToastModule,
+    GlobalToastComponent
+  ],
 })
 export class SharedModule { }
